116c41d9249f0b4a3de69599adf77e9341998fa2
Rish <codieefranchise@gmail.com>
2023-01-05 03:48:59 +0530

    jhfjhf

--- src/data/EventsData.js
+++ src/data/EventsData.js
@@ -1,41 +1,47 @@
 export const eventsData = [
 	{
-		img: '../asset/img/NITMUN1.jpg',
+		img: './assets/img/NITMUN1.jpg',
+		text: 'hello kaushal bhaia'
 	},
 	{
-		img: '../asset/img/NITMUN2.jpg',
+		img: './assets/img/NITMUN2.jpg',
 		class: 'tall',
+		text: 'I am pulling all-nighter to create this'
 	},
 
 	{
-		img: './assets/masonry3.jpg',
+		img: './assets/img/NITMUN3.jpg',
 	},
 	{
-		img: './assets/masonry11.jpg',
+		img: './assets/img/NITMUN4.jpg',
 		class: 'wide',
 	},
 	{
-		img: './assets/masonry4.jpg',
+		img: './assets/img/NITMUN5.jpg',
 		class: 'big',
 	},
 	{
-		img: './assets/masonry5.jpg',
+		img: './assets/img/NITMUN6.jpg',
 	},
 	{
-		img: './assets/masonry6.jpg',
+		img: './assets/img/NITMUN7.jpg',
 		class: 'tall',
 	},
 	{
-		img: './assets/masonry10.jpg',
+		img: './assets/img/NITMUN8.jpg',
 		class: 'tall',
 	},
 	{
-		img: './assets/masonry7.jpg',
+		img: './assets/img/NITMUN9.jpg',
+
 	},
 	{
-		img: './assets/masonry8.jpg',
+		img: './assets/img/NITMUN10.jpg',
 	},
 	{
-		img: './assets/masonry9.jpg',
+		img: './assets/img/NITMUN11.jpg',
+	},
+	{
+		img: './assets/img/NITMUN12.jpg',
 	},
 ];
