c0fd09f0b1411039a73ec3749d03eab77174db0b
Rish <codieefranchise@gmail.com>
2023-01-09 14:42:06 +0530

    responsive+navbar

--- src/data/HeroData.js
+++ src/data/HeroData.js
@@ -1,69 +1,68 @@
 export const heroMain = {
-	reverse: true,
-	inverse: false,
-	topLine: {
-		// text: 'Free',
-		// location: 'Park City, Utah',
-	},
-	headline: 'Find the best events',
-	description:
-		'',
-	buttonLabel: 'Study Guide',
-	buttonLabel1: 'Portfolio Matrix',
-	imgStart: 'start',
-	img: './assets/DISEC.jpg',
-	start: 'true',
+  reverse: true,
+  inverse: false,
+  topLine: {
+    // text: 'Free',
+    // location: 'Park City, Utah',
+  },
+  headline: "Find the best events",
+  description: "",
+  buttonLabel: "Study Guide",
+  buttonLabel1: "Portfolio Matrix",
+  imgStart: "start",
+  img: "./assets/DISEC.jpg",
+  start: "true",
 };
 
 const heroOne = {
-	reverse: true,
-	inverse: true,
-	topLine: {
-		text: 'Free',
-		location: 'Park City, Utah',
-	},
-	headline: 'UNGA-DISEC',
-	description:
-		'Welcome New Year with Wondergarden on waterfront. Celebrate with world-class international acts, roaming performers, children’s performers and entertainment',
-	buttonLabel: 'Study Guide',
-	buttonLabel1: 'Portfolio Matrix',
-	imgStart: 'start',
-	img: './assets/DISEC.png',
-	start: 'true',
+  reverse: true,
+  inverse: true,
+  topLine: {
+    text: "Free",
+    location: "Park City, Utah",
+  },
+  headline: "UNGA-DISEC",
+  description:
+    "Welcome New Year with Wondergarden on waterfront. Celebrate with world-class international acts, roaming performers, children’s performers and entertainment",
+  buttonLabel: "Study Guide",
+  buttonLabel1: "Portfolio Matrix",
+  imgStart: "start",
+  img: "./assets/DISEC.png",
+  start: "true",
 };
 
 const heroTwo = {
-	reverse: false,
-	inverse: false,
-	topLine: {
-		text: 'Paid',
-		location: 'Hard Rock Stadium, Miami, Florida',
-	},
-	headline: 'UNGA-SPECPOL',
-	description:
-		'Full of fresh, original and ground-breaking music, the festival’s set in the urban playground on the center.',
-	buttonLabel: 'Study guide',
-	buttonLabel1: 'Portfolio Matrix',
-	imgStart: 'start',
-	img: './assets/SPECPOL.png',
-	start: 'true',
+  reverse: false,
+  inverse: false,
+  topLine: {
+    text: "Paid",
+    location: "Hard Rock Stadium, Miami, Florida",
+  },
+  headline: "UNGA-SPECPOL",
+  description:
+    "Full of fresh, original and ground-breaking music, the festival’s set in the urban playground on the center.",
+  buttonLabel: "Study guide",
+  buttonLabel1: "Portfolio Matrix",
+  imgStart: "start",
+  img: "./assets/SPECPOL.png",
+  start: "true",
 };
 
 const heroThree = {
-	reverse: true,
-	inverse: true,
-	topLine: {
-		location: 'New Orleans, Louisiana',
-		text: 'Exclusive',
-	},
-	headline: 'AIPPM',
-	description:
-		'This two-week festival brings together a chocka social calendar and array of events including theatre and cabaret, music, visual arts exhibitions and installations, film and literature',
-	buttonLabel: 'Study Guide',
-	buttonLabel1: 'Portfolio Matrix',
-	imgStart: '',
-	img: './assets/AIPPM.jpg',
-	start: 'true',
+  reverse: true,
+  inverse: true,
+  topLine: {
+    location: "New Orleans, Louisiana",
+    text: "Exclusive",
+  },
+  headline: "AIPPM",
+  description:
+    "This two-week festival brings together a chocka social calendar and array of events including theatre and cabaret, music, visual arts exhibitions and installations, film and literature",
+  buttonLabel: "Study Guide",
+  buttonLabel1: "Portfolio Matrix",
+  imgStart: "",
+  img: "./assets/AIPPM.jpg",
+  start: "true",
 };
 
 const heroFour = {
@@ -77,7 +76,7 @@ const heroFour = {
   description:
     "Try a selection of signature dishes from finest restaurants as you stroll through most glamorous food festival.",
   buttonLabel: "Study Guide",
-  
+  buttonLabel1: "Portfolio Matrix",
 
   imgStart: "",
   img: "./assets/hero3.jpg",
